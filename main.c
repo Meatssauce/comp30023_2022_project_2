b539707ae24cc828e09cc44f0b4e26e656c520db
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2022-06-02 00:13:47 +1000

    added header files for windows but still has problems

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+//#ifdef _WIN32
+////For Windows
+//int betriebssystem = 1;
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <iphlpapi.h>
+//#include <ws2def.h>
+//#pragma comment(lib, "Ws2_32.lib")
+//#include <windows.h>
+//#include <io.h>
+//#else
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//#include <netdb.h>
+//int betriebssystem = 2;
+//#endif
+
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
